e3c3637701337d2bf2f6fd5bc7555b99f2c27455
Withanage Perera <prav.perera@gmail.com>
2025-11-24 12:19:14 -0500

    Spotted a bug in my counter design. Also finished the rest of the top_level design of 'data in'. Need to think about some testbenches before I move onto the top-top level.

--- Slave Design/Common/counter.sv
+++ Slave Design/Common/counter.sv
@@ -15,26 +15,15 @@ module counter #(
     output logic [WIDTH-1:0] count
 );
 
-    logic [WIDTH-1:0] next_count_r;
-
     // Updating count
     always_ff @(posedge FPGA_clk or posedge rst) begin
 
         if (rst)
             count <= '0;
+        else if (enable)
+            count <= count + 'd1;
         else
-            count <= next_count_r;
-
-    end
-
-    // Next count logic
-    always_comb begin
-
-        if (enable == 1'b1)
-            next_count_r <= count + 1;
-        else
-            next_count_r <= count;
-
+            count <= count;
     end
 
 endmodule
--- Slave Design/Data In/data_in_top_level.sv
+++ Slave Design/Data In/data_in_top_level.sv
@@ -17,8 +17,13 @@ module data_in_top_level #(
     input logic rst,
 
     output logic done,
+    output logic SDA_down,
     output logic [NUM_BYTES-1:0] HEX_out [3:0]
 );
+
+    logic byte_count, bit_count;
+    logic byte_en, bit_en;
+    logic byte_rst, bit_rst;
 
     logic [NUM_BYTES-1:0] received_data [7:0];
 
@@ -34,7 +39,40 @@ module data_in_top_level #(
         .rst(rst),
 
         .done(done),
-        .received_data(received_data)
-    )
+        .received_data(received_data),
+        .byte_en(byte_en),
+        .bit_en(bit_en),
+        .byte_rst(byte_rst),
+        .bit_rst(bit_rst),
+        .SDA_down(SDA_down)
+    );
+
+    // I need to map received data to our HEX outputs now.
+    // This isn't a great approach since I'm hardcoding the indices, but I allowed for parameters.
+    // I'll have to fix that in the future.
+    assign HEX_out[0] = received_data[5][3:0];
+    assign HEX_out[1] = received_data[4][3:0];
+    assign HEX_out[2] = received_data[3][3:0];
+    assign HEX_out[3] = received_data[2][3:0];
+    assign HEX_out[4] = received_data[1][3:0];
+    assign HEX_out[5] = received_data[0][3:0];
+
+    // Keeps track of which byte we're on
+    counter #(.WIDTH(NUM_BYTES)) byte_counter (
+        .FPGA_clk(FPGA_clk),
+        .enable(byte_en),
+        .rst(byte_rst || rst),
+
+        .count(byte_count)
+    );
+
+    // Keeps track of which bit we're on
+    counter #(.WIDTH(8)) bit_counter (
+        .FPGA_clk(FPGA_clk),
+        .enable(bit_en),
+        .rst(bit_rst || rst),
+
+        .count(bit_count)
+    );
 
 endmodule
